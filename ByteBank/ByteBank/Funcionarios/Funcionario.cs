5ba0611afa8d8ba4b3e05a9c004971b390590e2e
gilvan <g.r.almeida@live.com>
2020-08-16 22:18:36 -0300

    Construtor na classe base

--- ByteBank/ByteBank/Funcionarios/Funcionario.cs
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -1,10 +1,23 @@
-﻿namespace ByteBank.Funcionarios
+﻿using System;
+
+namespace ByteBank.Funcionarios
 {
     public class Funcionario
     {
+        public static int TotalDeFuncionarios { get; private set; }
+
         public string Nome { get; set; }
-        public string CPF { get; set; }
+        public string CPF { get; private set; }
         public double Salario { get; set; }
+
+        public Funcionario(string cpf)
+        {
+            Console.WriteLine("Criando FUNCIONARIO");
+
+            CPF = cpf;
+            
+            TotalDeFuncionarios++;
+        }
 
         public virtual double GetBonificacao()
         {       
