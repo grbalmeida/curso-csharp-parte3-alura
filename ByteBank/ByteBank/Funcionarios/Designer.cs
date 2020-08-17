ad5ab7998b48d91a7dc1f69464156896a4a1547c
gilvan <g.r.almeida@live.com>
2020-08-17 14:23:51 -0300

    Criando classes abstratas

--- ByteBank/ByteBank/Funcionarios/Designer.cs
+++ ByteBank/ByteBank/Funcionarios/Designer.cs
@@ -1,20 +1,20 @@
 ﻿namespace ByteBank.Funcionarios
 {
-    public class Diretor : Funcionario
+    public class Designer : Funcionario
     {
-        public Diretor(string cpf) : base(5000, cpf)
+        public Designer(string cpf) : base(3000, cpf)
         {
 
         }
 
         public override void AumentarSalario()
         {
-            Salario *= 1.15;
+            Salario *= 1.1;
         }
 
         public override double GetBonificacao()
         {
-            return Salario * 0.5;
+            return Salario * 0.17;
         }
     }
 }
--- ByteBank/ByteBank/Funcionarios/Funcionario.cs
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -1,8 +1,6 @@
-﻿using System;
-
-namespace ByteBank.Funcionarios
+﻿namespace ByteBank.Funcionarios
 {
-    public class Funcionario
+    public abstract class Funcionario
     {
         public static int TotalDeFuncionarios { get; private set; }
 
@@ -12,8 +10,6 @@ namespace ByteBank.Funcionarios
 
         public Funcionario(double salario, string cpf)
         {
-            Console.WriteLine("Criando FUNCIONARIO");
-
             CPF = cpf;
             Salario = salario;
             
